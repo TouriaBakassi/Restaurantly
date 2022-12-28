f74dfbbc9677bf0e837e334aeee2e75149742b9c
TouiaBakassi <Touria_Bakassi@outlook.com>
2022-12-28 22:40:47 +0100

    The sequel

---
+++ script.js
@@ -0,0 +1,10 @@
+let xmark=document.getElementById("xmark");
+let video=document.querySelector(".show-video");
+let btnvideo=document.querySelector(".video-btn");
+
+btnvideo.addEventListener("click",()=>{
+  video.classList.add("show");
+});
+xmark.addEventListener("click",()=>{
+video.classList.remove("show");
+});
