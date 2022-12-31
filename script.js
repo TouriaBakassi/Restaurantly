943d54783f21e4ee975de7d9ac0d4fbf641a5b10
TouiaBakassi <Touria_Bakassi@outlook.com>
2022-12-31 16:59:27 +0100

    The Sequel

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ xmark.addEventListener("click",()=>{
 video.classList.remove("show")
 });
 
-// ............Pour nav-cle 
-let allnavcle = document.querySelectorAll(".navcle");
-allnavcle.forEach((item)=>{
+// ............Pour nav-cle "active"
+let allnavCle = document.querySelectorAll(".navcle");
+allnavCle.forEach((item)=>{
   item.addEventListener("click",()=>{
-    allnavcle.forEach((item)=>{
+    allnavCle.forEach((item)=>{
       item.classList.remove("active");
     });
     item.classList.add("active");
   });
 });
 
-// .............Pour Menu-Cle 
+// .............Pour Menu-Cle "active"
 let allcle = document.querySelectorAll(".cle");
 
 allcle.forEach((item)=>{
@@ -29,5 +29,46 @@ allcle.forEach((item)=>{
    item.classList.remove("active");     
       } );
   item.classList.add("active");
+  });
+});
+
+// .......................Menu
+let all = document.querySelector(".all-item");
+let starters = document.querySelector(".starters");
+let salads = document.querySelector(".salads");
+let specialty = document.querySelector(".specialty");
+
+let allitem = document.querySelectorAll(".all");
+let allstarter = document.querySelectorAll(".starter");
+let allsalad = document.querySelectorAll(".salad");
+let allspecial = document.querySelectorAll(".special");
+
+all.addEventListener("click",()=>{
+  allitem.forEach((item)=>{
+    item.classList.remove("close");
+  });
+});
+starters.addEventListener("click",()=>{
+  allitem.forEach((item)=>{
+    item.classList.add("close");
+  });
+  allstarter.forEach((item)=>{
+    item.classList.remove("close");
+  });
+});
+salads.addEventListener("click",()=>{
+  allitem.forEach((item)=>{
+    item.classList.add("close");
+  });
+  allsalad.forEach((item)=>{
+    item.classList.remove("close");
+  });
+});
+specialty.addEventListener("click",()=>{
+  allitem.forEach((item)=>{
+    item.classList.add("close");
+  });
+  allspecial.forEach((item)=>{
+    item.classList.remove("close");
   });
 });
