ec3285708d0ffcb0ba0456fc01c484abbe7809a7
TouiaBakassi <Touria_Bakassi@outlook.com>
2022-12-31 23:17:36 +0100

    The Sequel

--- script.js
+++ script.js
@@ -72,3 +72,58 @@ specialty.addEventListener("click",()=>{
     item.classList.remove("close");
   });
 });
+// ..............................Specials-Key
+
+let allSpecialKey = document.querySelectorAll(".special-key");
+
+allSpecialKey.forEach((item)=>{
+  item.addEventListener("click",()=>{
+allSpecialKey.forEach((item)=>{
+  item.classList.remove("active");
+});
+item.classList.add("active");
+  });
+});
+// ...............................Specials-content
+let key1 = document.querySelector(".key1");
+let key2 = document.querySelector(".key2");
+let key3 = document.querySelector(".key3");
+let key4 = document.querySelector(".key4");
+let key5 = document.querySelector(".key5");
+let allcontent = document.querySelectorAll(".special-content")
+let content1 =document.querySelector(".content1");
+let content2 =document.querySelector(".content2");
+let content3 =document.querySelector(".content3");
+let content4 =document.querySelector(".content4");
+let content5 =document.querySelector(".content5");
+
+key1.addEventListener("click",()=>{
+allcontent.forEach((item)=>{
+  item.classList.remove("open");
+})
+content1.classList.add("open");
+});
+key2.addEventListener("click",()=>{
+  allcontent.forEach((item)=>{
+    item.classList.remove("open");
+  })
+  content2.classList.add("open");
+  });
+  key3.addEventListener("click",()=>{
+    allcontent.forEach((item)=>{
+      item.classList.remove("open");
+    })
+    content3.classList.add("open");
+  });
+  key4.addEventListener("click",()=>{
+    allcontent.forEach((item)=>{
+       item.classList.remove("open");
+     })
+      content4.classList.add("open");
+    });
+ key5.addEventListener("click",()=>{
+   allcontent.forEach((item)=>{
+     item.classList.remove("open");
+      })
+  content5.classList.add("open");
+    });    
